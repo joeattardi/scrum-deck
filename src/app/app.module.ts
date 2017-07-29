02f44dd405dafddcbb26c728f0c642db10eeb6a0
Joe Attardi <joe@attardi.net>
2017-07-29 11:40:35 -0400

    Initial commit, POC of cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { DeckComponent } from './deck/deck.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    DeckComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/deck/deck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deck',
+  templateUrl: './deck.component.html',
+  styleUrls: ['./deck.component.scss']
+})
+export class DeckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

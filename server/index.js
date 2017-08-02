a33ce326049c2db462489e2466bf37f2f341edcb
Joe Attardi <joe@attardi.net>
2017-08-02 19:41:45 -0400

    Add basic node server

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
+
+const port = process.env.PORT | 9000;
+const server = http.createServer(app);
+server.listen(port, () => console.log(`ScrumDeck listening on port ${port}`));

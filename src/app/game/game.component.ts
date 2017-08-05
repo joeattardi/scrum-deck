ebb0e825896ca1bf771c9ce648e8e0fc55800850
Joe Attardi <joe@attardi.net>
2017-08-04 23:07:58 -0400

    Make cards smaller, remove stacked effect

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { SocketService } from '../socket.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   flipped = true;
+
+  constructor(private socketService: SocketService) { }
+
+  ngOnInit() {
+    this.socketService.init();
+  }
 }

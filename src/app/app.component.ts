6f82875c09e425ee09a65e0aeb7ba420c0fb6d91
Joe Attardi <joe@attardi.net>
2017-08-05 20:01:05 -0400

    Add footer with version and link to GitHub page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { VERSION } from '../version';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  version = VERSION;
 }

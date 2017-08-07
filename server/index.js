cb6556ad7661b12a114fa199e60bb2e497419ec9
Joe Attardi <joe@attardi.net>
2017-08-07 18:30:13 -0400

    Fix logical OR with port number

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-const port = process.env.PORT | 9000;
+const port = process.env.PORT || 9000;
+
 const server = http.createServer(app);
 server.listen(port, () => console.log(`ScrumDeck listening on port ${port}`));
 
